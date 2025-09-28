eb33be23438b14ab3292d2ac41728411af58557d
Shane <69217993+CreatingCoder@users.noreply.github.com>
2025-09-28 17:17:40 -0500

    Create File_Encrypt.py

---
+++ File_Encrypt.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import Button
+
+
+window = tk.Tk()
+
+#Title of the window
+window.title('File Encryption/Decryption')
+
+#default window size
+window.geometry('600x400')
+
+#Background color
+window.configure(background='dark grey')
+
+#Top label
+label = tk.Label(window, text="Encrypt or Decrypt Files", font=("Arial", 16),  bg="dark grey")
+
+#Padding for Label
+label.pack(pady=0)
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#loop to keep the window open
+window.mainloop()
